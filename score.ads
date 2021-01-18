5b0a74c6d7cb40009944cb68d11889c351e7102a
xMacKow <maciek.k.k@op.pl>
2021-01-18 15:31:20 +0100

    scores

--- score.ads
+++ score.ads
@@ -1,6 +1,6 @@
 package score is
 	procedure SaveScore (value: in Integer);
-	entry Get (V: in out Integer; C: in out boolean);
+	entry Get (value: in out Integer);
 	score_value: Integer := 0;
 	is_different: boolean := false;
 end score;
